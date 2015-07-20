4361c4db493d085c64267bb02e5c66601d6ab5b3
Matt Snyder <matt@cranialpulse.com>
2015-07-20 11:42:13 -0400

    #mapsum exercise on page 69

--- my_list.exs
+++ my_list.exs
@@ -1,4 +1,16 @@
 defmodule MyList do
-	def sum([total | tail]) when length(tail) == 0, do: total
+	def sum([total]), do: total
 	def sum([ total | [ head | tail ]]), do: sum([ total + head ] ++ tail)
+
+	def reduce([], value, _) do
+		value
+	end
+	def reduce([head | tail], value, func) do
+		reduce(tail, func.(head, value), func)
+	end
+
+	def mapsum([head | tail], func), do: _mapsum(tail, func.(head), func)
+	defp _mapsum([], total, func), do: total
+	defp _mapsum([ head | tail ], total, func), do: _mapsum(tail, func.(head) + total, func)
+
 end
