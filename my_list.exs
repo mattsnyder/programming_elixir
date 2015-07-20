694e910fb3345bc347c3297d743b8b03b900cd13
Matt Snyder <matt@cranialpulse.com>
2015-07-20 12:57:47 -0400

    Caesar exercise on page 96

--- my_list.exs
+++ my_list.exs
@@ -11,12 +11,17 @@ defmodule MyList do
 
 	def mapsum([head | tail], func), do: _mapsum(tail, func.(head), func)
 	defp _mapsum([], total, func), do: total
-	defp _mapsum([ head | tail ], total, func), do: _mapsum(tail, func.(head) + total, func)
-
+	defp _mapsum([ head | tail ], total, func), do: _mapsum(tail, total + func.(head), func)
 
 	def max([head | tail]), do: _max(tail, head)
 	defp _max([], max), do: max
 	defp _max([head | tail], max) when head > max, do: _max(tail, head) 
 	defp _max([head | tail], max) when head <= max, do: _max(tail, max) 
 	
+	def caesar([head | tail], n), do: _caesar(tail, _morph(head, n), n)
+	defp _caesar([], morphed, _), do: morphed
+	defp _caesar([head | tail], morphed, n), do: _caesar(tail, morphed ++ _morph(head, n), n)
+	defp _morph(letter, inc) when [(letter + inc)] > 'z', do: _morph(letter - 26, inc)
+	defp _morph(letter, inc), do: [letter + inc]
+
 end
