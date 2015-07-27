92de741a47cc5b4e16c9b3e1387af3d2d4abd229
Matt Snyder <matt@cranialpulse.com>
2015-07-27 15:33:38 -0700

    Enum.filter exercise on page 92

--- my_enum.exs
+++ my_enum.exs
@@ -9,5 +9,12 @@ defmodule MyEnum do
 	def each([], _), do: []
 	def each([ head | tail], func), do: _each(tail, [func.(head)], func)
 	defp _each([ head | tail ], memo, func), do: _each(tail, memo ++ [func.(head)], func)
-	defp _each([], memo, func), do: memo
+	defp _each([], memo, _), do: memo
+
+	def filter([], _), do: []
+	def filter(list, func), do: _filter(list, [], func)
+	defp _filter([], filtered, _), do: filtered
+  defp _filter([ head | tail], filtered, func), do: _filter(tail, _append_if(head, filtered, func.(head)), func)
+	defp _append_if(item, list, true), do: list ++ [item]
+  defp _append_if(_, list, false), do: list
 end
